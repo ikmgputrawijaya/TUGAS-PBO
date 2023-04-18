2cc261f1377455b8d12a14b28efb65afe06cf43b
ikmgputrawijaya <113492234+ikmgputrawijaya@users.noreply.github.com>
2023-04-18 23:35:39 +0800

    Update Menu.java
    Commit akhir

--- src/Menu.java
+++ src/Menu.java
@@ -1,21 +1,26 @@
 import java.util.ArrayList;
 
 public class Menu {
-    //Method untuk mengembalikan nilai dari atribut idMenu pada objek Menu
     public int idMenu;
     public String namaMenu;
     public int hargaMenu;
+
     //Constructor kelas Menu
     public Menu(int idMenu, String namaMenu, int hargaMenu) {
         this.idMenu = idMenu;
         this.namaMenu = namaMenu;
         this.hargaMenu = hargaMenu;
     }
-    ///Mengembalikan nilai dari atribut nama dan alamat.
-    public int getIdMenu() { return idMenu; }
+
+    ///Mengembalikan nilai dari atribut harga, nama ,dan id.
+    public int getIdMenu() {
+        return idMenu;
+    }
+
     public String getNamaMenu() {
         return namaMenu;
     }
+
     public int getHargaMenu() {
         return hargaMenu;
     }

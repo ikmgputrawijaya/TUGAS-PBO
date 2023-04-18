db9916f681abffe78c383832d17f577b7ca80a46
ikmgputrawijaya <113492234+ikmgputrawijaya@users.noreply.github.com>
2023-04-18 23:34:36 +0800

    Update Pesanan.java
    Commit akhir

--- src/Pesanan.java
+++ src/Pesanan.java
@@ -5,24 +5,31 @@ public class Pesanan {
     private Menu menu;
     private int jumlah;
     private int jarakRumah;
+    private int jumlahOrder;
 
-    public Pesanan(Menu menu, int jumlahOrder) {
+    public Pesanan(Menu menu, int jumlahOrder, Restaurant restaurant) {
         this.menu = menu;
         this.jumlah = jumlahOrder;
     }
+
     public ArrayList<Pesanan> getDaftarPesanan() {
         return getDaftarPesanan();
     }
+
     public Menu getMenu() {
         return menu;
     }
+
+
+
     public int getJumlahOrder() {
-        return jumlah;
+        return jumlahOrder;
     }
     public int getJarakRumah() {
         return jarakRumah;
     }
     public int getTotalHarga() {
-        return menu.getHargaMenu() * jumlah + (jarakRumah/2);
+        return menu.getHargaMenu() * jumlahOrder + (jarakRumah/2);
     }
+
 }
